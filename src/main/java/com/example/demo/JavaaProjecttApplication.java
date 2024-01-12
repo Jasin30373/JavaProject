20de8dd564dee1ab826520a776cc40ff27da233d
Jasin30373 <ji30373@seeu.edu.mk>
2024-01-12 15:25:42 +0100

    init

---
+++ src/main/java/com/example/demo/JavaaProjecttApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaaProjecttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaaProjecttApplication.class, args);
+	}
+
+}
